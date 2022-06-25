b9d75c0d682245a6ac275d8888ab694670013887
Wilson Zanuncio <wzanuncio@gmail.com>
2022-06-25 12:04:45 -0400

    javascript parâmetros url página orçamento e efeito perguntas frequentes na página seguros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar efeito nos links do menu do cabeçalho
 const headerLinks = document.querySelectorAll(".header-menu a");
 
 if (headerLinks) {
@@ -12,3 +13,34 @@ if (headerLinks) {
 
   headerLinks.forEach(ativarLink);
 }
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativo");
+  const ativo = resposta.classList.contains("ativo");
+  pergunta.setAttribute("aria-expanded", ativo);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
